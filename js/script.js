bd928ce328bef3dfbbc34f8451889b8a209fb0be
Melvin Ernesto Santana Garcia <melvinernestosantanagarcia15@gmail.com>
2022-05-30 12:02:24 -0500

    Arreglando nombre navbar

--- js/script.js
+++ js/script.js
@@ -5,16 +5,16 @@ window.addEventListener ("scroll", function() {
 });
 
 let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
+let navbar = document.querySelector('.navlist');
 
 menu.onclick = () => {
 	menu.classList.toggle('bx-x');
-	navlist.classList.toggle('active');
+	navbar.classList.toggle('active');
 };
 
 window.onscroll = () => {
 	menu.classList.remove('bx-x');
-	navlist.classList.remove('active');
+	navbar.classList.remove('active');
 };
 
 const sr = ScrollReveal ({
