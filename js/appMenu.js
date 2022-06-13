dcfbbc33f619a3f1a71050f29e87847a56af7b84
Melvin Ernesto Santana Garcia <melvinernestosantanagarcia15@gmail.com>
2022-06-12 22:47:15 -0500

    Update

---
+++ js/appMenu.js
@@ -0,0 +1,29 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("sticky", window.scrollY > 0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navbar.classList.toggle('active');
+};
+
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navbar.classList.remove('active');
+};
+
+const sr = ScrollReveal ({
+	distance: '45px',
+	duration: 2700,
+	reset: true
+})
+
+sr.reveal('.home-text',{delay:350, origin:'left'})
+sr.reveal('.home-img',{delay:350, origin:'right'})
+
+sr.reveal('.home,.about,.sub-service,.portfolio,.service,.container__blogs,.container__skills,.container__Work,.container__other-portfolio,.container__slider3D,.about-stats,.container__timeline,.contact.,container__social,.end',{delay:200, origin:'bottom'})
