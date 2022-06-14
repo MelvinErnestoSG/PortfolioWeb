6264c8b6b84f3bf50f7b32544d526335ff522d93
Melvin Ernesto Santana Garcia <melvinernestosantanagarcia15@gmail.com>
2022-06-14 16:22:10 -0500

    Update section

--- js/appMenu.js
+++ js/appMenu.js
@@ -26,4 +26,4 @@ const sr = ScrollReveal ({
 sr.reveal('.home-text',{delay:350, origin:'left'})
 sr.reveal('.home-img',{delay:350, origin:'right'})
 
-sr.reveal('.sub-service,.about,.portfolio,.service,.contact',{delay:200, origin:'bottom'})
+sr.reveal('.about,.portfolio,.service,.sub-service,.blogs,.contact',{delay:200, origin:'bottom'})
